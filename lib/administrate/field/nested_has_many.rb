81f36c6fdadc6554f53787dc86c31341a3d5e24e
William Albright <ping@ewilli.am>
2018-03-05 14:33:49 -0800

    support namespaced models

--- lib/administrate/field/nested_has_many.rb
+++ lib/administrate/field/nested_has_many.rb
@@ -3,6 +3,9 @@ require "administrate"
 require "administrate/field/has_many"
 require "cocoon"
 
+# TODO: DRY up special_association_name and namespace
+# with :class_name and :field
+
 module Administrate
   module Field
     class NestedHasMany < Administrate::Field::HasMany
@@ -30,19 +33,25 @@ module Administrate
         data
       end
 
-      def self.dashboard_for_resource(resource)
-        "#{resource.to_s.classify}Dashboard".constantize
+      def self.dashboard_for_resource(resource, namespace = nil)
+        name = namespace.camelize.constantize if namespace
+
+        if name
+          "#{name}::#{resource.to_s.classify}Dashboard".constantize
+        else
+          "#{resource.to_s.classify}Dashboard".constantize
+        end
       end
 
-      def self.associated_attributes(associated_resource)
+      def self.associated_attributes(associated_resource, namespace)
         DEFAULT_ATTRIBUTES +
-          dashboard_for_resource(associated_resource).new.permitted_attributes
+          dashboard_for_resource(associated_resource, namespace).new.permitted_attributes
       end
 
       def self.permitted_attribute(associated_resource, options)
         {
           "#{associated_resource}_attributes".to_sym =>
-          associated_attributes(associated_resource),
+          associated_attributes(associated_resource, options[:namespace]),
         }
       end
 
@@ -51,6 +60,7 @@ module Administrate
       end
 
       def association_name
+        return special_association_name unless special_association_name.empty?
         associated_class_name.underscore.pluralize
       end
 
@@ -58,10 +68,18 @@ module Administrate
         Administrate::Page::Form.new(associated_dashboard, resource)
       end
 
+      def namespace_name
+        options[:namespace].to_s
+      end
+
       private
 
       def skipped_fields
         Array(options[:skip])
+      end
+
+      def special_association_name
+        options[:association_name].to_s
       end
     end
   end
